0a7f2c2333ddf93ab11df29a217d5b5c8557b83f
Stephanie Miller <smiller@generalcode.com>
2017-06-09 14:42:21 -0400

    Fix quote handling and pass all tests.
    Tests needed to be converted to use and expect svg files.
    Remote tests needed a remote svg file.
    Url handling regex needed to learn to look for " and ' around url and strip them.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@
 var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
-var util = require('util');
-var svgo = require('svgo');
+var SVGO = require('svgo');
 var Stream = require('stream').Stream;
 
 // NPM library
@@ -16,7 +15,7 @@ var buffers = require('buffers');
 var async = require('async');
 var chalk = require('chalk');
 
-var rImages = /url(?:\(['|"]?)(.*?.svg)(?:['|"]?\))(?!.*\/\*svg:skip\*\/)/ig;
+var rImages = /url((?:\(['|"]?)(.*?.svg(?:[#?][^"|']*)?)(?:['|"]?\)))(?!.*\/\*svg:skip\*\/)/ig;
 
 function gulpCssSvg(opts) {
   opts = JSON.parse(JSON.stringify(opts || {}));
@@ -24,13 +23,13 @@ function gulpCssSvg(opts) {
   opts.baseDir = opts.baseDir || '';
   opts.verbose = process.argv.indexOf('--verbose') !== -1;
 
-    // Creating a stream through which each file will pass
+  // Creating a stream through which each file will pass
   var stream = through.obj(function (file, enc, callbackStream) {
     var currentStream = this;
     var cache = [];
 
     if (file.isNull()) {
-            // Do nothing if no contents
+      // Do nothing if no contents
       currentStream.push(file);
 
       return callbackStream();
@@ -41,46 +40,46 @@ function gulpCssSvg(opts) {
       var result = [];
 
       async.whilst(
-                function () {
-                  result = rImages.exec(src);
+        function () {
+          result = rImages.exec(src);
 
-                  return result !== null;
-                },
-                function (callback) {
-                  if (cache[result[1]]) {
-                    src = src.replace(result[1], cache[result[1]]);
-                    callback();
-                    return;
-                  }
+          return result !== null;
+        },
+        function (callback) {
+          if (cache[result[2]]) {
+            src = src.replace(result[1], cache[result[2]]);
+            callback();
+            return;
+          }
 
-                  var pureUrl = result[1].split('?')[0].split('#')[0];
+          encodeResource(result[2], file, opts, function (fileRes) {
+            if (undefined !== fileRes) {
+              if (fileRes.contents.length > opts.maxWeightResource) {
+                log('Ignores ' + chalk.yellow(result[1]) + ', file is too big ' + chalk.yellow(fileRes.contents.length + ' bytes'), opts.verbose);
+                callback();
+                return;
+              }
 
-                  encodeResource(result[1], file, opts, function (fileRes) {
-                    if (undefined !== fileRes) {
-                      if (fileRes.contents.length > opts.maxWeightResource) {
-                        log('Ignores ' + chalk.yellow(result[1]) + ', file is too big ' + chalk.yellow(fileRes.contents.length + ' bytes'), opts.verbose);
-                        callback();
-                        return;
-                      }
+              var strRes = 'data:' + mime.lookup(fileRes.path) + ';charset=utf8,';
+              new SVGO().optimize(fileRes.contents.toString('utf8'), function (result) {
+                strRes += result.data.replace(/"/g, '\'').replace(/</g, '%3C').replace(/>/g, '%3E').replace(/{/g, '%7B').replace(/}/g, '%7D').replace(/#/g, '%23');
+              });
+              strRes = '("' + strRes + '")';
+              src = src.replace(result[1], strRes);
 
-                      var strRes = 'data:' + mime.lookup(fileRes.path) + ';charset=utf8,';
-										 new svgo().optimize(fileRes.contents.toString('utf8'), function(result) { strRes += result.data.replace(/"/g,"'").replace(/</g,'%3C').replace(/>/g,'%3E').replace(/{/g,'%7B').replace(/}/g,'%7D').replace(/#/g,'%23'); });
-										 strRes = '"' + strRes + '"';
-                      src = src.replace(result[1],  strRes );
+              // Store in cache
+              cache[result[2]] = strRes;
+            }
+            callback();
+          });
+        },
+        function () {
+          file.contents = new Buffer(src);
+          currentStream.push(file);
 
-                            // Store in cache
-                      cache[result[1]] = strRes;
-                    }
-                    callback();
-                  });
-                },
-                function () {
-                  file.contents = new Buffer(src);
-                  currentStream.push(file);
-
-                  return callbackStream();
-                }
-            );
+          return callbackStream();
+        }
+      );
     }
 
     if (file.isStream()) {
@@ -88,7 +87,7 @@ function gulpCssSvg(opts) {
     }
   });
 
-    // returning the file stream
+  // returning the file stream
   return stream;
 }
 
@@ -109,7 +108,7 @@ function encodeResource(img, file, opts, doneCallback) {
 
   if (/^(http|https|\/\/)/.test(img)) {
     log('Fetch ' + chalk.yellow(img), opts.verbose);
-        // different case for uri start '//'
+    // different case for uri start '//'
     if (img[0] + img[1] === '//') {
       img = 'http:' + img;
     }
@@ -129,7 +128,7 @@ function encodeResource(img, file, opts, doneCallback) {
     var location = '';
     var binRes = '';
 
-    location = img.charAt(0) === '/' ? (opts.baseDir || '') + img : path.join(path.dirname(file.path), (opts.baseDir || '') + '/' + img);
+    location = img.charAt(0) === '/' ? (opts.baseDir || '') + img : path.join(path.dirname(file.path || ''), (opts.baseDir || '') + '/' + img);
     location = location.replace(/([?#].*)$/, '');
 
     if (!fs.existsSync(location)) {
@@ -167,7 +166,7 @@ function fetchRemoteRessource(url, callback) {
       return;
     }
 
-        // Bail if we get anything other than 200
+    // Bail if we get anything other than 200
     if (response.statusCode !== 200) {
       callback(null);
       return;
